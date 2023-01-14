24532200c4cf2b2a85d1caf04f0c2e1f761b399f
aritejhg <59247531+aritejhg@users.noreply.github.com>
2023-01-14 16:42:26 +0800

    added test imgs and requirements.txt

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
 
 
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, Filters
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction
-
 # creating 
